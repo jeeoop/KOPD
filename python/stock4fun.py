30e0af8ef915824ee315b31fcaa2f6f3695b0db7
Yongjian (Ken) Ouyang <ken.oyang@gmail.com>
2015-05-26 15:24:17 -0500

    improvement

--- python/stock4fun.py
+++ python/stock4fun.py
@@ -9,13 +9,20 @@ Created on Wed Apr 29 10:56:48 2015
 import urllib
 
 def stockCheck(symbol):
-    base_url = 'http://finance.google.com/finance?q='
-    content = urllib.urlopen(base_url + symbol).read()
-    location = content.lower().find('values:["' + symbol + '"')
-    [price,increase] = content[location:location+200].split('","')[2:4]
+    if symbol.lower() == 'oil':
+        base_url = 'http://www.bloomberg.com/energy/'
+        content = urllib.urlopen(base_url).read()
+        location = content.find("(WTI)</td>\n<td>USD/bbl.")
+        [price,na,increase]=content[location:location+200]\
+        .replace('\n','').replace('<td>', '|').replace('</td>','|').replace('<td class="up">','|').replace('<td class="down">','|').split('|')[4:7]
+    else:
+        base_url = 'http://finance.google.com/finance?q='
+        content = urllib.urlopen(base_url + symbol).read()
+        location = content.lower().find('values:["' + symbol + '"')
+        [price,increase] = content[location:location+200].split('","')[2:4]
     print price, increase
 
-
+stockCheck('oil')
 stockCheck('aapl')
 stockCheck('nrg')
 stockCheck('goog')
@@ -29,3 +36,7 @@ stockCheck('lnkd')
 #ontent = urllib.urlopen(base_url + symbol).read()
 #location = content.lower().find('values:["' + symbol + '"')
 #content[location-50:location+50]
+
+
+
+
